b7bceb1c7b9cd1ed8c775bc53dce209f8871a830
Joey Long <feemplton@outlook.com>
2025-12-13 15:34:20 -0600

    Added conf file

---
+++ conf.py
@@ -0,0 +1,46 @@
+# file to write to /etc/httpd/conf.d/x.conf file
+
+# define constants
+CONF = "/etc/httpd/conf.d"
+LOG = "/var/log/httpd"
+
+# TODO: remove whitespaces and replace with tabs
+def update_http_conf(HTTP, f_mk_prompt, ROOT):
+    w_http = f""" 
+    <VirtualHost *:{HTTP}>
+      ServerName    {f_mk_prompt}
+      DocumentRoot  {ROOT}/{f_mk_prompt}
+      ErrorLog      {LOG}/{f_mk_prompt}.err
+    </VirtualHost>
+    """
+    
+    # write to file
+    with open(f"{CONF}/{f_mk_prompt}.conf", "w") as file:
+        try:
+            file.write(w_http)
+        # most likely to occur
+        except PermissionError as e:
+            print(f"Error occured: {e}")
+
+    print("HTTP configuration successful.")
+
+
+# do the same but for https
+def update_https_conf(HTTPS, f_mk_prompt, ROOT):
+    w_https = f""" 
+    <VirtualHost *:{HTTPS}>
+      ServerName    {f_mk_prompt}
+      DocumentRoot  {ROOT}/{f_mk_prompt}
+      ErrorLog      {LOG}/{f_mk_prompt}.err
+    </VirtualHost>
+    """
+    
+    # write to file
+    with open(f"{CONF}/{f_mk_prompt}.conf", "w") as file:
+        try:
+            file.write(w_http)
+        # most likely to occur
+        except PermissionError as e:
+            print(f"Error occured: {e}")
+
+    print("HTTPS configuration successful.")
